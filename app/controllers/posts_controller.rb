767588d7b5f31922f87d1b9fab726ae8295ff978
Daniel Au <dakmau@gmail.com>
2015-02-21 04:27:49 -0500

    Fix create posts, update show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def create
@@ -23,6 +24,6 @@ class PostsController < ApplicationController
 
 
   def whitelisted_post_params
-
+    params.require(:post).permit(:title, :author_name, :body)
   end
 end
