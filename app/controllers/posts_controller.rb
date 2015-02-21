43d32577b6af7f24d5786990f73c40786f2d7ec5
Daniel Au <dakmau@gmail.com>
2015-02-21 04:18:28 -0500

    Create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(whitelisted_post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+
+  def whitelisted_post_params
+
+  end
 end
