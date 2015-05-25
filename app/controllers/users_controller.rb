0a4d6d3a13c99a7bf54238044b4cc61069dd90de
telwell <me@trevorelwell.com>
2015-05-24 22:23:38 -0400

    RESTful methods up and running. Flash notices working. All systems GO

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	include UsersHelper
+
 	def index
 		@users = User.all
 	end
@@ -12,14 +14,34 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		@user = User.new
-		@user.first_name = params[:user][:first_name]
-		@user.last_name = params[:user][:last_name]
-		@user.bio = params[:user][:bio]
+		@user = User.new(user_params)
 		if @user.save
+			flash.notice = "User '#{@user.first_name} #{@user.last_name}' Created!"
 			redirect_to user_path(@user)
 		else
 			:new
 		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.update(user_params)
+
+		flash.notice = "User '#{@user.first_name} #{@user.last_name}' Updated!"
+
+		redirect_to user_path(@user)
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+
+		flash.notice = "User '#{@user.first_name} #{@user.last_name}' Deleted!"
+
+		redirect_to users_path
 	end
 end
