2ceda805084703e7dcd4342402c33b1bc6faaa24
Adam <adam.kinson@gmail.com>
2015-07-03 14:15:16 -0400

    finish Create & Show

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,4 +6,32 @@ class NotesController < ApplicationController
   end
 
 
+  def create
+    @note = Note.new(whitelisted_params)
+
+    if @note.save
+      # flash success
+      redirect_to note_path(@note.id)
+    else
+      # flash warning
+      render :new
+    end
+
+
+    def show
+      @note = Note.find(params[:id])
+    end
+
+  end
+
+
+
+  private
+
+
+  def whitelisted_params
+    params.require(:note).permit(:category, :body)
+  end
+
+
 end
