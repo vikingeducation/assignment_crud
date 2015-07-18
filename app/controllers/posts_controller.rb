9c7a1c787d890856ef8da0afae6d5149cb1c7d42
alokp <alokpra@gmail.com>
2015-07-18 10:18:08 -0700

    complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
 
   def new
@@ -17,11 +18,30 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(whitelist_params)
+      redirect_to post_path(@post.id)
+    else
+      flash[:error] = "Did not update!"
+      render :edit
+    end
+  end
+
 private
 
   def whitelist_params
     params.require(:post).permit(:title, :body)
   end
-
 
 end
