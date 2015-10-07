126f0560230996480662be8ae28b1a79cafd72e9
Justin Mullis <justin.mullis@gmail.com>
2015-10-07 16:34:55 -0400

    move kitten image to model

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,5 +1,3 @@
-require 'kitten_url'
-
 class KittensController < ApplicationController
 
   def new
@@ -9,7 +7,7 @@ class KittensController < ApplicationController
 
   def create
     @kitten = Kitten.new(kitten_params)
-    @kitten.picture = KittenUrl::get_image
+
     if @kitten.save
       redirect_to @kitten
     else
--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,13 @@
+require 'kitten_url'
+
 class Kitten < ActiveRecord::Base
+
+  before_create :get_image
+
+  private
+
+  def get_image
+    self.picture = KittenUrl::get_image
+  end
+
 end
