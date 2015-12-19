dc0a124bfa6491ccebb8f29094061a0be622aeca
Sia <siakaramalegos@gmail.com>
2015-12-19 12:16:11 -0600

    Adds new and create actions

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,23 @@
 class DogsController < ApplicationController
+  before_action :set_dog, only: [:edit, :update, :destroy, :show]
+
+  def show
+  end
+
   def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+
+    if @dog.save
+      flash[:notice] = "Dog successfully created!"
+      redirect_to @dog
+    else
+      flash[:error] = "Hmm, something went wrong. Try again."
+      redirect_to :new
+    end
   end
 
   def index
@@ -7,5 +25,15 @@ class DogsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def set_dog
+    @dog = Dog.find(params[:id])
+  end
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
   end
 end
