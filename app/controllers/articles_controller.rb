d1da3295877546a3cc8de4ae90958303e4126bc9
Thomas Lo <thomasjinlo@gmail.com>
2016-01-26 09:05:47 -0800

    Created new and index page

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,15 @@
+class ArticlesController < ApplicationController
+
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.find(params[:])
+  end
+end
