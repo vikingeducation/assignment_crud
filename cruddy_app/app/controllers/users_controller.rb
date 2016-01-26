5feec0569ca5377f090293bcda6731cf75a301f2
Joshua Masland-Sarani <j.masland@gmail.com>
2016-01-26 13:18:36 -0500

    finish cruddy app

--- cruddy_app/app/controllers/users_controller.rb
+++ cruddy_app/app/controllers/users_controller.rb
@@ -6,8 +6,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(whitelisted_user_params)
-    @user.save
-    redirect_to user_path(@user.id)
+    if @user.save
+      flash[:notice] = "Successfully created a new user!"
+      redirect_to user_path(@user.id)
+    else
+      flash[:notice] = "Oh no! Something went wrong."
+      redirect_to users_path
+    end
   end
 
   def edit
@@ -16,8 +21,13 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.save
-    redirect_to user_path(@user.id)
+    if @user.update( whitelisted_user_params )
+      flash[:notice] = "Successfully created a new user!"
+      redirect_to user_path(@user.id)
+    else
+      flash[:notice] = "Oh no! Something went wrong."
+      redirect_to users_path
+    end
   end
 
   def show
@@ -29,14 +39,16 @@ class UsersController < ApplicationController
   end
 
   def destroy
-
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "User destroyed!"
+    redirect_to users_path
   end
 
   private
+
   def whitelisted_user_params
     params.require(:user).permit(:first_name, :last_name, :email)
   end
-
-
 
 end
--- cruddy_app/app/models/user.rb
+++ cruddy_app/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
