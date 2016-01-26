1c5c702efc65474dbfc06f2b055fa298559b93dc
Kelsey James <kelseybjames@gmail.com>
2016-01-26 09:33:18 -0800

    finished restful methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,34 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.find(params[:])
+    @article = Article.new(whitelisted_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(whitelisted_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def whitelisted_params
+    params.require(:article).permit(:title, :body)
   end
 end
