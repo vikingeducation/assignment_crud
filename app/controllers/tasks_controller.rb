43489e6a3993e240f522a5374c519d33f26778aa
Steven Chang <prime_pork@hotmail.com>
2016-02-27 14:38:07 +1000

    Story: Indexing Tassks (read) done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,10 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    # we just need to pass it in the variabel with the id
+    @task = Task.find(params[:id])
+  end
 end
